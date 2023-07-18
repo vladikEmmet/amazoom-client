59ed0511e3f889154cc06513649bcfb4c588be49
Владислав Объедков <vladislavobedkov@MBP-Vladislav.Dlink>
2023-07-18 21:48:31 +0600

    3

--- pages/admin/admin-categories.tsx
+++ pages/admin/admin-categories.tsx
@@ -2,12 +2,13 @@ import AdminOperations from "@/components/AdminOperations";
 import { NextPageAuth } from "@/providers/authProvider/auth-page.types"
 import Layout from "@/components/UI/Layout/Layout";
 import Meta from "@/components/UI/Meta";
+import { AdminTypesEnum } from "@/types/AdminOperations";
 
 const AdminCategories: NextPageAuth = () => {
   return (
     <Meta title="Categories management">
       <Layout>
-        <AdminOperations type="category"/>
+        <AdminOperations type={AdminTypesEnum.CATEGORY}/>
       </Layout>
     </Meta>
   )
--- pages/admin/admin-products.tsx
+++ pages/admin/admin-products.tsx
@@ -2,12 +2,13 @@ import AdminOperations from "@/components/AdminOperations";
 import { NextPageAuth } from "@/providers/authProvider/auth-page.types"
 import Layout from "@/components/UI/Layout/Layout";
 import Meta from "@/components/UI/Meta";
+import { AdminTypesEnum } from "@/types/AdminOperations";
 
 const AdminProducts: NextPageAuth = () => {
   return (
     <Meta title="Products management">
       <Layout>
-        <AdminOperations type="product"/>
+        <AdminOperations type={AdminTypesEnum.PRODUCT}/>
       </Layout>
     </Meta>
   )
